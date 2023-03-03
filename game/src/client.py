d222558ceeaabc7135e87af8b341bf96dd05c17f
jvjue <799525644@qq.com>
2023-03-03 22:23:37 +0800

    finish client

--- game/src/client.py
+++ game/src/client.py
@@ -6,8 +6,10 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
+# 用于接收终端信息
+from sys import stdin
 
-def main():
+def operate(op, user_id, username, score):
     # Make socket
     transport = TSocket.TSocket('localhost', 9090)
 
@@ -23,13 +25,20 @@ def main():
     # Connect!
     transport.open()
 
-    user = User(1, 'JUE', 1500)
-    client.add_user(user, "")
-
+    user = User(user_id, username, score)
+    if op == "add":
+        client.add_user(user, "")
+    elif op == "remove":
+        client.remove_user(user, "")
     # Close!
     transport.close()
 
-# 当直接调用时执行该语句，不直接调用时不执行
+# 定义main函数从
+def main():
+    for line in stdin:
+        op, user_id, username, score = line.split(' ')
+        operate(op, int(user_id), username, int(score))
+
+# __name__获取执行对象，当直接调用时执行该语句，不直接调用时不执行
 if __name__ == "__main__":
     main()
-
